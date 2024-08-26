02bf71835919b60de0434270f91f4116c5d5a98d
NotDhalix <adga2002@hotmail.com>
2024-08-26 15:14:54 -0500

    adding dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LogIn from './LogIn';
 import SignUp from './SignUp';
+import Dashboard from './dashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LogIn />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<LogIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
